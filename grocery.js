6dc2ce17c3420f32e3903cf81ff53802d89c0276
JPW <jpw@hawaii.edu>
2021-09-19 17:15:02 -1000

    starter challenge

---
+++ grocery.js
@@ -0,0 +1,19 @@
+var itemData = [
+       { category: 'fruit',  itemName: 'apple', onSale: false },
+       { category: 'canned', itemName: 'beans', onSale: false },
+       { category: 'canned', itemName: 'corn',  onSale: true  },
+       { category: 'frozen', itemName: 'pizza', onSale: false },
+       { category: 'fruit',  itemName: 'melon', onSale: true  },
+       { category: 'canned', itemName: 'soup',  onSale: false },
+];
+
+
+
+const organizeItems = (items) => {
+  //write your stuff here
+  return "hello"
+};
+
+
+
+console.log(organizeItems( itemData ));
